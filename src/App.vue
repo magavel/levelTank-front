b794f32a8bd8dbea93008e9249aa460ee0be06be
feron <avel.feron@gmail.com>
2021-04-19 09:21:55 +0200

    mise a neuf de l'install vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      Niveau cuve à eau
-
-      <v-spacer></v-spacer>
-    </v-app-bar>
-    <v-main>
-      <chart></chart>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-import Chart from '@/components/chart.vue';
+#nav {
+  padding: 30px;
 
-export default {
-  name: 'App',
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-  components: {
-    Chart,
-  },
-
-  data: () => ({
-    //
-  }),
-};
-</script>
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
